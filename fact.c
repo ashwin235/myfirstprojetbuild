dc3633e32e021e7d4dd9efcb0c1f88d2f22e9bd6
ashwin <ashwindevops235@gmail.com>
2021-04-23 08:11:37 +0000

    fact file changed

--- fact.c
+++ fact.c
@@ -7,6 +7,6 @@ scanf("%d",n);
     for(i=1;i<=n;i++){    
       fact=fact*i;    
   }    
-  printf("Factorial of %d is: %d",number,fact);    
+  printf("Factorial of %d is: %d",n,fact);    
 
 }   
